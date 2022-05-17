d229b08104e2903f1f55d7423ca9d04e160eec92
Somrage <somrage@gmail.com>
2022-05-17 16:38:43 +0700

    fixed grid creation with js

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 const table = document.querySelector('.table')
 
-let grids = 50
+let grids = 15
 
-for (let i = 0; i < grids; i++) {
+for (let i = 0; i < grids*grids; i++) {
     const grid = document.createElement('div')
     grid.classList.add('grid')
     table.appendChild(grid);
 }
+
+table.style.cssText = `grid-template-columns: repeat(${grids}, 1fr); grid-template-rows: repeat(${grids}, 1fr);`
